ed8d2104a679e25b77f82bed57a8add0b7608ad8
Dileep0103 <dileepkumarvalluri16@gmail.com>
2026-07-05 22:18:03 +0530

    Removed temporary 4 testing files

--- backend/dns-test.js
+++
@@ -1,9 +0,0 @@
-const dns = require("dns");
-
-dns.resolveSrv("_mongodb._tcp.cluster0.ueweef6.mongodb.net", (err, records) => {
-  if (err) {
-    console.error("SRV Error:", err);
-    return;
-  }
-  console.log(records);
-});
